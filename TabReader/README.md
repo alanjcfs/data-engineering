6cbaf169c8311a54035fd9e15ad70f356a095cb3
Alan Fung-Schwarz <alan@atomsign.net>
2014-03-20 11:51:57 -0700

    Created Rails application

---
+++ TabReader/README.md
@@ -0,0 +1,4 @@
+# Tab Reader
+
+Tab Reader is a simple application that takes a user-submitted file and parses
+it into the relational database.
---
+++ TabReader/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TabReader::Application.config.secret_key_base = 'fc06cbc0b9ac089ac566ebd05bdc1c8365cf0a8727c1ef9262f3cab02b9b275c577e45ec86e6f29d73de5cd509fbf4c11ba0546595e87eadb6e838dab41acbd7'

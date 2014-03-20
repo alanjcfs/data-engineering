cf6d0fdb4cb767e7d32951968716a97cabe37f2e
Alan Fung-Schwarz <alan@atomsign.net>
2014-03-20 12:50:14 -0700

    Add multipart for form and test params[:file]

--- TabReader/app/controllers/files_controller.rb
+++ TabReader/app/controllers/files_controller.rb
@@ -1,2 +1,13 @@
 class FilesController < ApplicationController
+  def new
+
+  end
+
+  def create
+    logger.info params[:file].inspect
+
+    tab_file = params[:file].read
+
+    redirect_to new_file_path
+  end
 end

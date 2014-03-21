b398908bc82afd66e0a83db5b3cf26cab6395499
Alan Fung-Schwarz <alan@atomsign.net>
2014-03-20 17:13:28 -0700

    Show gross revenue after uploading and cleanup

--- TabReader/app/controllers/files_controller.rb
+++ TabReader/app/controllers/files_controller.rb
@@ -3,10 +3,8 @@ class FilesController < ApplicationController
   end
 
   def create
-    logger.info params[:file].inspect
-
-    tabfile = params[:file].read
-    @tabfile = tabfile.split("\n")
+    file = params[:file].read
+    @tabfile = file.split("\n")
 
     # Ignore the first line
     @tabfile[1..-1].each do |tabbed_data|
@@ -17,10 +15,14 @@ class FilesController < ApplicationController
   end
 
   def index
-    @items = Item.all
+    @items = Item.includes(:merchant).all
     @purchasers = Purchaser.all
     @merchants = Merchant.all
-    @items_purchasers = ItemsPurchaser.all
+    @items_purchasers = ItemsPurchaser.includes(:item, :purchaser).all
+
+    @gross_revenue = @items_purchasers.reduce(0) { |accumulator, items_purchased|
+      accumulator + items_purchased.price * items_purchased.count
+    } / 100.0
 
     respond_to do |format|
       format.html
@@ -34,7 +36,8 @@ class FilesController < ApplicationController
     # merchant address & merchant name
     merchant = Merchant.where(address: data[4], name: data[5]).first_or_create
     # item description & item price
-    item = merchant.items.where("items.description LIKE ?", "%#{data[1]}%").first_or_create(price: (data[2].to_f * 100).to_i, description: data[1])
+    item = merchant.items.where("items.description LIKE ?", "%#{data[1]}%").
+      first_or_create(price: (data[2].to_f * 100).to_i, description: data[1])
 
     # purchase count
     ItemsPurchaser.create(count: data[3], purchaser_id: purchaser.id, item_id: item.id)
--- TabReader/app/models/items_purchaser.rb
+++ TabReader/app/models/items_purchaser.rb
@@ -1,4 +1,6 @@
 class ItemsPurchaser < ActiveRecord::Base
   belongs_to :item
   belongs_to :purchaser
+
+  delegate :price, to: :item
 end
